c42cec8582b5ea2c55e3d2762f8b31cf4840869c
Shehryar Kamran <kool_dude1996@live.com>
2018-12-10 21:49:40 +0500

    Initial commit

---
+++ app/src/main/java/com/ShehryarKamran/textreccy/TextRecognization.java
@@ -0,0 +1,16 @@
+package com.ShehryarKamran.textreccy;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+
+public class TextRecognization extends Application {
+
+    public static final  String RESULT_TEXT = "RESULT_TEXT";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseApp.initializeApp(this);
+    }
+}
